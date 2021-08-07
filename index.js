3dd96ed8e50ffd1354f9c34f86962e2acab0ff07
Assassin#1234 <chordsandfrets@gmail.com>
2021-08-08 00:27:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const discord = require("discord.js");
-require("dotenv").config();
-
-const client = new discord.Client({
-    allowedMentions: { parse: ['users'], repliedUser: false },
-    intents: [discord.Intents.FLAGS.GUILDS, discord.Intents.FLAGS.GUILD_MESSAGES, discord.Intents.FLAGS.DIRECT_MESSAGES, discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, discord.Intents.FLAGS.GUILD_MEMBERS, discord.Intents.FLAGS.GUILD_WEBHOOKS],
-    partials: ["MESSAGE", "REACTION", "CHANNEL"]
-});
-client.commands = new discord.Collection();
-let handlers = ["command_handler", "event_handler"];
-for (const handler of handlers) {
-    require(`./handlers/${handler}`)(client);
-};
-
-client.login(process.env.token);
+require('dotenv').config();
+const TCOCClient = require('./src/Structures/TCOCClient');
+const config = require('./src/Structures/TCOCConfig');
+const client = new TCOCClient(config);
+client.connect();
