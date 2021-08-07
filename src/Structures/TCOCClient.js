6e69096a73487a6a42db5a00b59a6e10da69c898
Assassin#1234 <chordsandfrets@gmail.com>
2021-08-08 00:27:14 +0530

    Create TCOCClient.js

---
+++ src/Structures/TCOCClient.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-unused-vars */
+const { Client, Collection, Permissions, Options } = require('discord.js');
+const TCOCDatabase = require('./TCOCDatabase.js');
+const Utils = require('./TCOCUtils.js');
+
+module.exports = class TCOCClient extends Client {
+	constructor(options = {}) {
+		super({
+			intents: options.intents,
+			allowedMentions: { parse: ['users', 'roles'], repliedUser: false },
+			shards: 'auto',
+			failIfNotExists: false,
+			messageCacheLifetime: 60,
+			messageSweepInterval: 1500,
+			makeCache: Options.cacheWithLimits({
+				MessageManager: 2,
+				PresenceManager: 0,
+				UserManager: 0,
+			}),
+		});
+
+		this.validate(options);
+
+		this.commands = new Collection();
+		this.aliases = new Collection();
+		this.slashCommands = new Collection();
+
+		this.events = new Collection();
+
+		this.cooldowns = new Collection();
+		this.subCommandsCooldowns = new Collection();
+		this.slashCooldowns = new Collection();
+
+		this.utils = new Utils(this);
+		this.db = new TCOCDatabase(this);
+
+	}
+
+	validate(options) {
+		if (typeof options !== 'object') { throw new TypeError('Options should be a type of Object.'); }
+
+		if (!options.token) {throw new Error('You must pass the token for the client.');}
+		this.token = options.token;
+
+		if (typeof options.prefix !== 'string') {throw new TypeError('Prefix should be a type of String.');}
+		this.prefix = options.prefix;
+
+		if (!options.developers) {throw new Error('You must pass a developer id(s) for the client.');}
+		this.developers = options.developers;
+
+		if (!options.defaultClientPerms) {throw new Error('You must pass default permissions for the client.');}
+		this.defaultClientPerms = new Permissions(options.defaultClientPerms).freeze();
+	}
+
+	async connect(token = this.token) {
+		this.db.loadDatabase();
+		this.utils.loadEvents();
+		this.utils.loadCommands();
+		await super.login(token);
+	}
+};
