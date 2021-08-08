c3456d9090c2f875170cf9c478b97620a81b722e
tovade <gamingethan650@gmail.com>
2021-08-08 11:26:52 +0200

    feat(Interactions) slash commands

---
+++ src/Events/interactionCreate.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+// const { MessageEmbed } = require('discord.js');
+const Event = require("../Structures/EventBase");
+
+module.exports = class extends Event {
+  async run(interaction) {
+    if (interaction.isCommand()) {
+      await this.client.application?.commands
+        .fetch(interaction.commandId)
+        .catch(() => {});
+      if (!interaction.guildId) return;
+      try {
+        const command = this.client.interactions.get(interaction.command?.name);
+        if (!command) return;
+        return await command.run(
+          interaction,
+          interaction?.options._options.map((value) => value.value)
+        );
+      } catch (err) {
+        interaction.reply({
+          content: "Something went terribly wrong.",
+          ephemeral: true,
+        });
+        console.log(err);
+      }
+    }
+  }
+};
---
+++ src/Interactions/Information/ping.js
@@ -0,0 +1,29 @@
+const InteractionBase = require("../../Structures/InteractionBase");
+const { Interaction, MessageEmbed } = require("discord.js");
+module.exports = class PingInteraction extends InteractionBase {
+  constructor(...args) {
+    super(...args, {
+      name: "ping",
+      description: "Check my ping!",
+    });
+  }
+  /**
+   * @param {Interaction} interaction
+   */
+  async run(interaction) {
+    return interaction.reply({
+      embeds: [
+        new MessageEmbed()
+          .setTitle("Pong!")
+          .setDescription(
+            [
+              `API -- ${this.client.utils.codeBlock(
+                Math.round(this.client.ws.ping) + "ms"
+              )}`,
+            ].join("\n")
+          )
+          .setFooter(this.client.user.tag, this.client.user.avatarURL()),
+      ],
+    });
+  }
+};
