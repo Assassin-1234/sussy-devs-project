9d4ab49f08fd5ec6765ab45fbb1cb98a70956156
tovade <gamingethan650@gmail.com>
2021-08-08 18:43:46 +0200

    feat(commands) Add invalid command corrector

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -1,73 +1,148 @@
 /* eslint-disable no-unused-vars */
 // const { MessageEmbed } = require('discord.js');
-const Event = require('../Structures/EventBase');
+const Event = require("../Structures/EventBase");
 
 module.exports = class extends Event {
-	async run(message) {
+  async run(message) {
+    const mentionRegexPrefix = RegExp(`^<@!?${this.client.user.id}> `);
 
-		const mentionRegexPrefix = RegExp(`^<@!?${this.client.user.id}> `);
+    if (!message.guild || message.author.bot) return;
 
-		if (!message.guild || message.author.bot) return;
+    const guildData = await this.client.db.findOrCreateGuild(message.guild.id);
 
-		const guildData = await this.client.db.findOrCreateGuild(message.guild.id);
+    if (guildData.blacklist.state === true) return;
 
-		if (guildData.blacklist.state === true) return;
+    const customPrefix = guildData.prefix
+      ? guildData.prefix
+      : this.client.prefix;
 
-		const customPrefix = guildData.prefix ? guildData.prefix : this.client.prefix;
+    const prefix = message.content.match(mentionRegexPrefix)
+      ? message.content.match(mentionRegexPrefix)[0]
+      : customPrefix;
 
-		const prefix = message.content.match(mentionRegexPrefix)
-			? message.content.match(mentionRegexPrefix)[0]
-			: customPrefix;
+    if (!message.content.startsWith(prefix)) return;
 
-		if (!message.content.startsWith(prefix)) return;
+    const [cmd, ...args] = message.content
+      .slice(prefix.length)
+      .trim()
+      .split(/ +/g);
 
-		const [cmd, ...args] = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command =
+      this.client.commands.get(cmd.toLowerCase()) ||
+      this.client.commands.get(this.client.aliases.get(cmd.toLowerCase()));
+    if (command) {
+      if (command.disabled) {
+        message.reply({
+          content: [
+            `\`${command.name}\` is globally disabled! Please check back later.`,
+          ].join("\n"),
+          components: [
+            {
+              type: 1,
+              components: [
+                {
+                  type: 2,
+                  style: 5,
+                  label: "Support Server",
+                  url: "https://discord.gg/EcqW7SHWVR",
+                },
+              ],
+            },
+          ],
+        });
+        return;
+      }
 
-		const command =
-            this.client.commands.get(cmd.toLowerCase()) ||
-            this.client.commands.get(this.client.aliases.get(cmd.toLowerCase()));
-		if (command) {
+      if (args.length > 0 && command.subCommands) {
+        const subcommand = args.slice().shift().toLowerCase();
 
-			if (command.disabled) {
-				message.reply({
-					content: [`\`${command.name}\` is globally disabled! Please check back later.` ].join('\n'),
-					components: [{ type: 1, components: [{ type: 2, style: 5, label: 'Support Server', url: 'https://discord.gg/EcqW7SHWVR' }] }],
-				});
-				return;
-			}
+        const foundSubcommand = command.subCommands.find((subCmd) => {
+          return (
+            subCmd.name === subcommand || subCmd.aliases?.includes(subcommand)
+          );
+        });
 
-			if (args.length > 0 && command.subCommands) {
-				const subcommand = args.slice().shift().toLowerCase();
-
-				const foundSubcommand = command.subCommands.find((subCmd) => {
-					return subCmd.name === subcommand || subCmd.aliases?.includes(subcommand);
-				});
-
-				if (foundSubcommand) {
-					const check = await this.client.utils.checkCommand(message, foundSubcommand, true);
-					if (check === false) return;
-					return command[foundSubcommand.name](message, args, customPrefix).catch((err) => {
-						message.reply({
-							content: [`Something went wrong with \`${command.name}\`'s \`${foundSubcommand.name}\` subcommand 😔` ].join('\n'),
-							components: [{ type: 1, components: [{ type: 2, style: 5, label: 'Support Server', url: 'https://discord.gg/EcqW7SHWVR' }] }],
-						});
-						console.log(err);
-						return;
-					});
-				}
-			}
-			const check = await this.client.utils.checkCommand(message, command);
-			if (check === false) return;
-			return command.run(message, args, customPrefix).catch((err) => {
-				message.reply({
-					content: [
-						`Something went wrong with \`${command.name}\` command 😔`,
-					].join('\n'),
-					components: [{ type: 1, components: [{ type: 2, style: 5, label: 'Support Server', url: 'https://discord.gg/EcqW7SHWVR' }] }],
-				});
-				console.log(err);
-				return;
-			});
-		}
-	}
+        if (foundSubcommand) {
+          const check = await this.client.utils.checkCommand(
+            message,
+            foundSubcommand,
+            true
+          );
+          if (check === false) return;
+          return command[foundSubcommand.name](
+            message,
+            args,
+            customPrefix
+          ).catch((err) => {
+            message.reply({
+              content: [
+                `Something went wrong with \`${command.name}\`'s \`${foundSubcommand.name}\` subcommand 😔`,
+              ].join("\n"),
+              components: [
+                {
+                  type: 1,
+                  components: [
+                    {
+                      type: 2,
+                      style: 5,
+                      label: "Support Server",
+                      url: "https://discord.gg/EcqW7SHWVR",
+                    },
+                  ],
+                },
+              ],
+            });
+            console.log(err);
+            return;
+          });
+        }
+      }
+      const check = await this.client.utils.checkCommand(message, command);
+      if (check === false) return;
+      return command.run(message, args, customPrefix).catch((err) => {
+        message.reply({
+          content: [
+            `Something went wrong with \`${command.name}\` command 😔`,
+          ].join("\n"),
+          components: [
+            {
+              type: 1,
+              components: [
+                {
+                  type: 2,
+                  style: 5,
+                  label: "Support Server",
+                  url: "https://discord.gg/EcqW7SHWVR",
+                },
+              ],
+            },
+          ],
+        });
+        console.log(err);
+        return;
+      });
+    } else {
+      const leven = require("../Utilities/Leven");
+      const best = [
+        ...this.client.commands.map((cmd) => cmd.name),
+        ...this.client.aliases.keys(),
+      ].filter(
+        (c) => leven(cmd.toLowerCase(), c.toLowerCase()) < c.length * 0.4
+      );
+      const dym =
+        best.length == 0
+          ? ""
+          : best.length == 1
+          ? `- ${cmd}\n+ ${best[0]}`
+          : `${best
+              .slice(0, 3)
+              .map((value) => `- ${cmd}\n+ ${value}`)
+              .join("\n")}`;
+      return dym
+        ? message.channel.send({
+            content: `Invalid command! Maybe you meant this command: \n \`\`\`diff\n${dym}\`\`\``,
+          })
+        : null;
+    }
+  }
 };
