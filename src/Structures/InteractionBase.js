a4ec83732bd4a91fa5ec1e1715d1a956a1ffc80c
Assassin#1234 <chordsandfrets@gmail.com>
2021-08-08 22:28:48 +0530

    lint + code cleanup

--- src/Structures/InteractionBase.js
+++ src/Structures/InteractionBase.js
@@ -1,11 +1,11 @@
 module.exports = class Interaction {
-  constructor(client, name, options = {}) {
-    this.client = client;
-    this.name = options.name || name;
-    this.description = options.description || "No description provided.";
-    this.options = options.options || [];
-  }
-  async run(interaction, args) {
-    throw new Error(`Interaction ${this.name} doesn't provide a run method`);
-  }
+	constructor(client, name, options = {}) {
+		this.client = client;
+		this.name = options.name || name;
+		this.description = options.description || 'No description provided.';
+		this.options = options.options || [];
+	}
+	async run(interaction, args) {
+		throw new Error(`Interaction ${this.name} doesn't provide a run method`);
+	}
 };
--- src/Utilities/Leven.js
+++ src/Utilities/Leven.js
@@ -1,88 +1,76 @@
-"use strict";
+/* eslint-disable no-mixed-spaces-and-tabs */
 const array = [];
 const charCodeCache = [];
 
 const leven = (left, right) => {
-  if (left === right) {
-    return 0;
-  }
+	if (left === right) {
+		return 0;
+	}
 
-  const swap = left;
+	const swap = left;
+	if (left.length > right.length) {
+		left = right;
+		right = swap;
+	}
 
-  // Swapping the strings if `a` is longer than `b` so we know which one is the
-  // shortest & which one is the longest
-  if (left.length > right.length) {
-    left = right;
-    right = swap;
-  }
-
-  let leftLength = left.length;
-  let rightLength = right.length;
-
-  // Performing suffix trimming:
-  // We can linearly drop suffix common to both strings since they
-  // don't increase distance at all
-  // Note: `~-` is the bitwise way to perform a `- 1` operation
-  while (
-    leftLength > 0 &&
+	let leftLength = left.length;
+	let rightLength = right.length;
+	while (
+		leftLength > 0 &&
     left.charCodeAt(~-leftLength) === right.charCodeAt(~-rightLength)
-  ) {
-    leftLength--;
-    rightLength--;
-  }
+	) {
+		leftLength--;
+		rightLength--;
+	}
+	let start = 0;
 
-  // Performing prefix trimming
-  // We can linearly drop prefix common to both strings since they
-  // don't increase distance at all
-  let start = 0;
-
-  while (
-    start < leftLength &&
+	while (
+		start < leftLength &&
     left.charCodeAt(start) === right.charCodeAt(start)
-  ) {
-    start++;
-  }
+	) {
+		start++;
+	}
 
-  leftLength -= start;
-  rightLength -= start;
+	leftLength -= start;
+	rightLength -= start;
 
-  if (leftLength === 0) {
-    return rightLength;
-  }
+	if (leftLength === 0) {
+		return rightLength;
+	}
 
-  let bCharCode;
-  let result;
-  let temp;
-  let temp2;
-  let i = 0;
-  let j = 0;
+	let bCharCode;
+	let result;
+	let temp;
+	let temp2;
+	let i = 0;
+	let j = 0;
 
-  while (i < leftLength) {
-    charCodeCache[i] = left.charCodeAt(start + i);
-    array[i] = ++i;
-  }
+	while (i < leftLength) {
+		charCodeCache[i] = left.charCodeAt(start + i);
+		array[i] = ++i;
+	}
 
-  while (j < rightLength) {
-    bCharCode = right.charCodeAt(start + j);
-    temp = j++;
-    result = j;
+	while (j < rightLength) {
+		bCharCode = right.charCodeAt(start + j);
+		temp = j++;
+		result = j;
 
-    for (i = 0; i < leftLength; i++) {
-      temp2 = bCharCode === charCodeCache[i] ? temp : temp + 1;
-      temp = array[i];
-      // eslint-disable-next-line no-multi-assign
-      result = array[i] =
+		for (i = 0; i < leftLength; i++) {
+			temp2 = bCharCode === charCodeCache[i] ? temp : temp + 1;
+			temp = array[i];
+			// eslint-disable-next-line no-multi-assign
+			result = array[i] =
         temp > result
-          ? temp2 > result
-            ? result + 1
-            : temp2
-          : temp2 > temp
-          ? temp + 1
-          : temp2;
-    }
-  }
+        	? temp2 > result
+        		? result + 1
+        		: temp2
+        	: temp2 > temp
+        		? temp + 1
+        		: temp2;
+		}
+	}
 
-  return result;
+	return result;
 };
 
 module.exports = leven;
