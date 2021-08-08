2968184a0b6e331f68b6e375651bba325a4f6dab
tovade <gamingethan650@gmail.com>
2021-08-08 19:05:23 +0200

    fix(Code) lint everything

--- src/Events/Client/ready.js
+++ src/Events/Client/ready.js
@@ -1,38 +1,38 @@
-const Event = require("../../Structures/EventBase.js");
+const Event = require('../../Structures/EventBase.js');
 
 module.exports = class extends Event {
-  constructor(...args) {
-    super(...args, {
-      once: true,
-    });
-  }
+	constructor(...args) {
+		super(...args, {
+			once: true,
+		});
+	}
 
-  async run() {
-    this.client.utils.loadInteractions();
+	async run() {
+		this.client.utils.loadInteractions();
 
-    await this.client.utils.sleep(500);
-    console.log(
-      [
-        `Logged in as ${this.client.user.tag}`,
-        `Loaded ${this.client.commands.size} commands & ${this.client.events.size} events & ${this.client.interactions.size} interactions!`,
-        `Ready in ${this.client.guilds.cache.size} guilds on ${
-          this.client.channels.cache.size
-        } channels, for a total of ${this.client.guilds.cache.reduce(
-          (a, b) => a + b.memberCount,
-          0
-        )} users.`,
-      ].join("\n")
-    );
+		await this.client.utils.sleep(500);
+		console.log(
+			[
+				`Logged in as ${this.client.user.tag}`,
+				`Loaded ${this.client.commands.size} commands & ${this.client.events.size} events & ${this.client.interactions.size} interactions!`,
+				`Ready in ${this.client.guilds.cache.size} guilds on ${
+					this.client.channels.cache.size
+				} channels, for a total of ${this.client.guilds.cache.reduce(
+					(a, b) => a + b.memberCount,
+					0,
+				)} users.`,
+			].join('\n'),
+		);
 
-    this.client.user.setPresence({
-      activities: [
-        {
-          type: "WATCHING",
-          name: `@${this.client.user.username} help | \nServing ${this.client.guilds.cache.size} Servers`,
-        },
-      ],
-    });
+		this.client.user.setPresence({
+			activities: [
+				{
+					type: 'WATCHING',
+					name: `@${this.client.user.username} help | \nServing ${this.client.guilds.cache.size} Servers`,
+				},
+			],
+		});
 
-    await this.client.utils.sleep(2000);
-  }
+		await this.client.utils.sleep(2000);
+	}
 };
--- src/Structures/BotUtils.js
+++ src/Structures/BotUtils.js
@@ -1,269 +1,267 @@
 /* eslint-disable no-unused-vars */
-const path = require("path");
-const { promisify } = require("util");
-const glob = promisify(require("glob"));
-const Command = require("./CommandBase.js");
-const Event = require("./EventBase.js");
-const Interaction = require("./InteractionBase");
-const { Collection, MessageEmbed, Message } = require("discord.js");
+const path = require('path');
+const { promisify } = require('util');
+const glob = promisify(require('glob'));
+const Command = require('./CommandBase.js');
+const Event = require('./EventBase.js');
+const Interaction = require('./InteractionBase');
+const { Collection, MessageEmbed, Message } = require('discord.js');
 module.exports = class Tools {
-  constructor(client) {
-    this.client = client;
-  }
+	constructor(client) {
+		this.client = client;
+	}
 
-  get directory() {
-    return `${path.dirname(require.main.filename)}${path.sep}`;
-  }
+	get directory() {
+		return `${path.dirname(require.main.filename)}${path.sep}`;
+	}
 
-  sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
+	sleep(ms) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
 
-  isClass(input) {
-    return (
-      typeof input === "function" &&
-      typeof input.prototype === "object" &&
-      input.toString().substring(0, 5) === "class"
-    );
-  }
+	isClass(input) {
+		return (
+			typeof input === 'function' &&
+      typeof input.prototype === 'object' &&
+      input.toString().substring(0, 5) === 'class'
+		);
+	}
 
-  codeBlock(string, code) {
-    if (code) return `\`\`\`${code}\n${string}\n\`\`\``;
-    return `\`\`\`\n${string}\n\`\`\``;
-  }
+	codeBlock(string, code) {
+		if (code) return `\`\`\`${code}\n${string}\n\`\`\``;
+		return `\`\`\`\n${string}\n\`\`\``;
+	}
 
-  checkDevs(target) {
-    return this.client.developers.includes(target);
-  }
+	checkDevs(target) {
+		return this.client.developers.includes(target);
+	}
 
-  formatArray(array, type = "conjunction") {
-    return new Intl.ListFormat("en-GB", { style: "short", type: type }).format(
-      array
-    );
-  }
+	formatArray(array, type = 'conjunction') {
+		return new Intl.ListFormat('en-GB', { style: 'short', type: type }).format(
+			array,
+		);
+	}
 
-  timer(timestamp) {
-    const timeLeft = timestamp - Date.now();
-    const days = Math.floor(timeLeft / 86400000);
-    const hours = Math.floor(timeLeft / 3600000) - days * 24;
-    const minutes = Math.floor(timeLeft / 60000) - days * 1440 - hours * 60;
-    const seconds =
+	timer(timestamp) {
+		const timeLeft = timestamp - Date.now();
+		const days = Math.floor(timeLeft / 86400000);
+		const hours = Math.floor(timeLeft / 3600000) - days * 24;
+		const minutes = Math.floor(timeLeft / 60000) - days * 1440 - hours * 60;
+		const seconds =
       Math.floor(timeLeft / 1000) - days * 86400 - hours * 3600 - minutes * 60;
-    const mseconds =
+		const mseconds =
       timeLeft / 1000 - days * 86400 - hours * 3600 - minutes * 60;
-    let string = "";
-    if (days) string = string + `${days} ${days == 1 ? "day" : "days"}`;
-    if (hours) string = string + `${hours} ${hours == 1 ? "hour" : "hours"}`;
-    if (minutes) {
-      string = string + `${minutes} ${minutes == 1 ? "minute" : "minutes"}`;
-    }
-    if (seconds) {
-      string = string + `${seconds} ${seconds == 1 ? "second" : "seconds"}`;
-    }
-    if (!string.length) string = `${mseconds.toFixed(1)} second`;
-    return string;
-  }
+		let string = '';
+		if (days) string = string + `${days} ${days == 1 ? 'day' : 'days'}`;
+		if (hours) string = string + `${hours} ${hours == 1 ? 'hour' : 'hours'}`;
+		if (minutes) {
+			string = string + `${minutes} ${minutes == 1 ? 'minute' : 'minutes'}`;
+		}
+		if (seconds) {
+			string = string + `${seconds} ${seconds == 1 ? 'second' : 'seconds'}`;
+		}
+		if (!string.length) string = `${mseconds.toFixed(1)} second`;
+		return string;
+	}
 
-  async checkCommand(message, command, subCommand = false) {
-    if (command.devsOnly && !this.checkDevs(message.author.id)) {
-      return false;
-    }
+	async checkCommand(message, command, subCommand = false) {
+		if (command.devsOnly && !this.checkDevs(message.author.id)) {
+			return false;
+		}
 
-    if (command.userPerms) {
-      const missing = message.channel
-        .permissionsFor(message.member)
-        .missing(command.userPerms);
-      if (missing.length) {
-        message.reply({
-          content: `You don't have \`${this.formatArray(
-            missing.map(this.client.tools.formatPerms)
-          )}\` permission to run \`${command.name}\` command!`,
-        });
-        return false;
-      }
-    }
-    const clientPermCheck = command.clientPerms
-      ? this.client.defaultClientPerms.add(command.clientPerms)
-      : this.client.defaultClientPerms;
-    if (clientPermCheck) {
-      const missing = message.channel
-        .permissionsFor(message.guild.me)
-        .missing(clientPermCheck);
-      if (missing.length) {
-        message.reply({
-          content: `I don't have \`${this.formatArray(
-            missing.map(this.client.tools.formatPerms)
-          )}\` permission to run \`${command.name}\` command!`,
-        });
-        return false;
-      }
-    }
+		if (command.userPerms) {
+			const missing = message.channel
+				.permissionsFor(message.member)
+				.missing(command.userPerms);
+			if (missing.length) {
+				message.reply({
+					content: `You don't have \`${this.formatArray(
+						missing.map(this.client.tools.formatPerms),
+					)}\` permission to run \`${command.name}\` command!`,
+				});
+				return false;
+			}
+		}
+		const clientPermCheck = command.clientPerms
+			? this.client.defaultClientPerms.add(command.clientPerms)
+			: this.client.defaultClientPerms;
+		if (clientPermCheck) {
+			const missing = message.channel
+				.permissionsFor(message.guild.me)
+				.missing(clientPermCheck);
+			if (missing.length) {
+				message.reply({
+					content: `I don't have \`${this.formatArray(
+						missing.map(this.client.tools.formatPerms),
+					)}\` permission to run \`${command.name}\` command!`,
+				});
+				return false;
+			}
+		}
 
-    if (command.disabled) {
-      message.reply({
-        content: [
-          `\`${command.name}\` **is globally disabled! Please check back later**`,
-        ].join("\n"),
-        components: [
-          {
-            type: 1,
-            components: [
-              {
-                type: 2,
-                style: 5,
-                label: "Support Server",
-                url: "https://discord.gg/EcqW7SHWVR",
-              },
-            ],
-          },
-        ],
-      });
-      return false;
-    }
+		if (command.disabled) {
+			message.reply({
+				content: [
+					`\`${command.name}\` **is globally disabled! Please check back later**`,
+				].join('\n'),
+				components: [
+					{
+						type: 1,
+						components: [
+							{
+								type: 2,
+								style: 5,
+								label: 'Support Server',
+								url: 'https://discord.gg/EcqW7SHWVR',
+							},
+						],
+					},
+				],
+			});
+			return false;
+		}
 
-    if (!subCommand) {
-      if (!message.client.cooldowns.has(command.name)) {
-        await message.client.cooldowns.set(command.name, new Collection());
-      }
-    } else if (!message.client.subCommandsCooldowns.has(command.name)) {
-      await message.client.subCommandsCooldowns.set(
-        command.name,
-        new Collection()
-      );
-    }
+		if (!subCommand) {
+			if (!message.client.cooldowns.has(command.name)) {
+				await message.client.cooldowns.set(command.name, new Collection());
+			}
+		}
+		else if (!message.client.subCommandsCooldowns.has(command.name)) {
+			await message.client.subCommandsCooldowns.set(
+				command.name,
+				new Collection(),
+			);
+		}
 
-    const now = Date.now();
-    const timestamps = !subCommand
-      ? message.client.cooldowns.get(command.name)
-      : message.client.subCommandsCooldowns.get(command.name);
-    const cooldownAmount = command.cooldown;
-    if (timestamps.has(message.author.id)) {
-      const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-      if (now < expirationTime) {
-        const timeLeft = this.timer(expirationTime);
-        message
-          .reply({
-            content: `This command is in cooldown for **${timeLeft}**`,
-          })
-          .catch(() => {
-            // something
-          });
-        return false;
-      }
-    }
+		const now = Date.now();
+		const timestamps = !subCommand
+			? message.client.cooldowns.get(command.name)
+			: message.client.subCommandsCooldowns.get(command.name);
+		const cooldownAmount = command.cooldown;
+		if (timestamps.has(message.author.id)) {
+			const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+			if (now < expirationTime) {
+				const timeLeft = this.timer(expirationTime);
+				message
+					.reply({
+						content: `This command is in cooldown for **${timeLeft}**`,
+					})
+					.catch(() => {
+						// something
+					});
+				return false;
+			}
+		}
 
-    timestamps.set(message.author.id, now);
-    return setTimeout(
-      () => timestamps.delete(message.author.id),
-      cooldownAmount
-    );
-  }
-  /**
+		timestamps.set(message.author.id, now);
+		return setTimeout(
+			() => timestamps.delete(message.author.id),
+			cooldownAmount,
+		);
+	}
+	/**
    * @param {Message} message - Message Object
    * @param {String} prefix - Guild prefix
    * @param {Object} comamnd - Comamnd ran
    * @param {Number} missingIndex - Missing argument index
    * @param {String} message - Error message
    */
-  async missingArgs(message, prefix, command, missingIndex, errorReply) {
-    const mapped = command.usage.map((arg) => arg).join(" ");
+	async missingArgs(message, prefix, command, missingIndex, errorReply) {
+		const mapped = command.usage.map((arg) => arg).join(' ');
 
-    const arrows = command.usage
-      .map((x, i) =>
-        missingIndex !== i
-          ? " ".repeat(x.length)
-          : " " + "^".repeat(x.length - 2) + " "
-      )
-      .join("");
-    const subExtra = command.parent ? command.parent.length + 1 : 0;
-    const codeBlock =
-      "```xml\n" +
-      `${prefix}${command.parent ? command.parent + " " : ""}${
-        command.name
-      } ${mapped}\n${" ".repeat(
-        subExtra + prefix.length + command.name.length + missingIndex
+		const arrows = command.usage
+			.map((x, i) =>
+				missingIndex !== i
+					? ' '.repeat(x.length)
+					: ' ' + '^'.repeat(x.length - 2) + ' ',
+			)
+			.join('');
+		const subExtra = command.parent ? command.parent.length + 1 : 0;
+		const codeBlock =
+      '```xml\n' +
+      `${prefix}${command.parent ? command.parent + ' ' : ''}${command.name} ${mapped}\n${' '.repeat(subExtra + prefix.length + command.name.length + missingIndex,
       )} ${arrows}\n${errorReply}` +
-      "```";
+      '```';
 
-    return message.reply({
-      embeds: [
-        new MessageEmbed()
-          .setAuthor(
-            message.author.username,
-            message.author.avatarURL({ dynamic: true })
-          )
-          .setColor("RED")
-          .setDescription(codeBlock)
-          .addField("Example", `\`${prefix}${command.example}\``, false),
-      ],
-    });
-  }
+		return message.reply({
+			embeds: [
+				new MessageEmbed()
+					.setAuthor(
+						message.author.username,
+						message.author.avatarURL({ dynamic: true }),
+					)
+					.setColor('RED')
+					.setDescription(codeBlock)
+					.addField('Example', `\`${prefix}${command.example}\``, false),
+			],
+		});
+	}
 
-  async loadCommands() {
-    return glob(`${this.directory}/src/Commands/**/*.js`).then((commands) => {
-      for (const commandFile of commands) {
-        delete require.cache[commandFile];
-        const { name } = path.parse(commandFile);
-        const File = require(commandFile);
-        if (!this.isClass(File)) {
-          throw new TypeError(`Command ${name} doesn't export a class.`);
-        }
-        const command = new File(this.client, name.toLowerCase());
-        if (!(command instanceof Command)) {
-          throw new TypeError(`Command ${name} doesn't belong in Commands.`);
-        }
-        this.client.commands.set(command.name, command);
-        if (command.aliases.length) {
-          for (const alias of command.aliases) {
-            this.client.aliases.set(alias, command.name);
-          }
-        }
-      }
-    });
-  }
-  async loadInteractions() {
-    return glob(`${this.directory}/src/Interactions/**/*.js`).then(
-      (interactions) => {
-        for (const interactionFile of interactions) {
-          delete require.cache[interactionFile];
-          const { name } = path.parse(interactionFile);
-          const File = require(interactionFile);
-          if (!this.isClass(File))
-            throw new TypeError(`Interaction ${name} doesn't export a class.`);
-          const interaction = new File(this.client, name.toLowerCase());
-          if (!(interaction instanceof Interaction))
-            throw new TypeError(
-              `Interaction ${name} doesn't belong in Interactions directory.`
-            );
-          this.client.interactions.set(interaction.name, interaction);
-          this.client.devMode
-            ? this.client.guilds.cache
-                .get("873573318836490280")
-                .commands.create(interaction)
-            : this.client.application?.commands.create(interaction);
-        }
-      }
-    );
-  }
+	async loadCommands() {
+		return glob(`${this.directory}/src/Commands/**/*.js`).then((commands) => {
+			for (const commandFile of commands) {
+				delete require.cache[commandFile];
+				const { name } = path.parse(commandFile);
+				const File = require(commandFile);
+				if (!this.isClass(File)) {
+					throw new TypeError(`Command ${name} doesn't export a class.`);
+				}
+				const command = new File(this.client, name.toLowerCase());
+				if (!(command instanceof Command)) {
+					throw new TypeError(`Command ${name} doesn't belong in Commands.`);
+				}
+				this.client.commands.set(command.name, command);
+				if (command.aliases.length) {
+					for (const alias of command.aliases) {
+						this.client.aliases.set(alias, command.name);
+					}
+				}
+			}
+		});
+	}
+	async loadInteractions() {
+		return glob(`${this.directory}/src/Interactions/**/*.js`).then(
+			(interactions) => {
+				for (const interactionFile of interactions) {
+					delete require.cache[interactionFile];
+					const { name } = path.parse(interactionFile);
+					const File = require(interactionFile);
+					if (!this.isClass(File)) {throw new TypeError(`Interaction ${name} doesn't export a class.`);}
+					const interaction = new File(this.client, name.toLowerCase());
+					if (!(interaction instanceof Interaction)) {
+						throw new TypeError(
+							`Interaction ${name} doesn't belong in Interactions directory.`,
+						);
+					}
+					this.client.interactions.set(interaction.name, interaction);
+					this.client.devMode
+						? this.client.guilds.cache
+							.get('873573318836490280')
+							.commands.create(interaction)
+						: this.client.application?.commands.create(interaction);
+				}
+			},
+		);
+	}
 
-  async loadEvents() {
-    return glob(`${this.directory}/src/Events/**/*.js`).then((events) => {
-      for (const eventFile of events) {
-        delete require.cache[eventFile];
-        const { name } = path.parse(eventFile);
-        const File = require(eventFile);
-        if (!this.isClass(File)) {
-          throw new TypeError(`Event ${name} doesn't export a class!`);
-        }
-        const event = new File(this.client, name);
-        if (!(event instanceof Event)) {
-          throw new TypeError(
-            `Event ${name} doesn't belong in Event directory.`
-          );
-        }
-        this.client.events.set(event.name, event);
-        event.emitter[event.type](name, (...args) => event.run(...args));
-      }
-    });
-  }
+	async loadEvents() {
+		return glob(`${this.directory}/src/Events/**/*.js`).then((events) => {
+			for (const eventFile of events) {
+				delete require.cache[eventFile];
+				const { name } = path.parse(eventFile);
+				const File = require(eventFile);
+				if (!this.isClass(File)) {
+					throw new TypeError(`Event ${name} doesn't export a class!`);
+				}
+				const event = new File(this.client, name);
+				if (!(event instanceof Event)) {
+					throw new TypeError(
+						`Event ${name} doesn't belong in Event directory.`,
+					);
+				}
+				this.client.events.set(event.name, event);
+				event.emitter[event.type](name, (...args) => event.run(...args));
+			}
+		});
+	}
 };
