73fce1f9ac21baa4a634ba510537079300c94157
RaZe <75721601+RaZegame@users.noreply.github.com>
2021-08-13 22:12:57 +0300

    Context menu handler

--- src/Events/interactionCreate.js
+++ src/Events/interactionCreate.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 // const { MessageEmbed } = require('discord.js');
+const { CommandInteraction } = require('discord.js');
 const Event = require('../Structures/EventBase');
 
 module.exports = class extends Event {
@@ -15,6 +16,26 @@ module.exports = class extends Event {
 				return await command.run(
 					interaction,
 					interaction?.options._options.map((value) => value.value),
+				);
+			}
+			catch (err) {
+				interaction.reply({
+					content: 'Something went terribly wrong.',
+					ephemeral: true,
+				});
+				console.log(err);
+			}
+		}
+		if(interaction.isContextMenu()) {
+			await this.client.application?.commands
+				.fetch(interaction.commandId)
+				.catch(() => { });
+			if (!interaction.guildId) return;
+			try {
+				const command = this.client.interactions.get(interaction.command?.name);
+				if (!command) return;
+				return await command.run(
+					interaction,
 				);
 			}
 			catch (err) {
