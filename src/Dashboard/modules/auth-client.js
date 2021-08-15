1dc0563f96c7e4b011b9a6e1c0022952494988e9
Assassin#1234 <chordsandfrets@gmail.com>
2021-08-15 12:47:27 +0530

    dashboard delete

--- src/Dashboard/modules/auth-client.js
+++
@@ -1,7 +0,0 @@
-const OAuthClient = require('disco-oauth');
-const settings = require('../../Structures/BotConfig');
-const client = new OAuthClient(settings.id, settings.secret);
-client.setRedirect(`${settings.dashboard}/auth`);
-client.setScopes('identify', 'guilds');
-
-module.exports = client;
